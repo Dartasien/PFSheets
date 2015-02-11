92228084f18040bacc6a2de82be4ca03180771e0
Dartasien <hamptfe@hotmail.com>
2015-02-11 14:40:46 -0500

    Original commit.

---
+++ PFSheets/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PFSheets.Startup))]
+namespace PFSheets
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

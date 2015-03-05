4ac953cdfe323a027d756771c1eadfcd79e96fbb
Dartasien <hamptfe@hotmail.com>
2015-03-05 08:32:26 -0500

    Fixed some login issues and set CreateNewCharacter page for registered users only.

--- PFSheets/App_Start/IdentityConfig.cs
+++ PFSheets/App_Start/IdentityConfig.cs
@@ -50,7 +50,7 @@ namespace PFSheets
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
